63e89e08dd6326e8d578d10f09238c18fe8dec85
Nathan Matos <nathanmatos38@gmail.com>
2026-04-10 20:27:16 -0300

    feat: sistema de login simples

---
+++ script.js
@@ -0,0 +1,26 @@
+const inputNome = document.getElementById('nome')
+let inputIdade = document.getElementById('idade')
+let inputSenha = document.getElementById('senha')
+let resultado = document.getElementById('res')
+const botao = document.getElementById('btn1')
+
+botao.addEventListener("click", ()=> {
+let idade = Number(inputIdade.value)
+let senha = inputSenha.value
+resultado.classList.remove("erro", "sucesso")
+
+if (inputNome.value === "" || inputIdade.value === "" || inputSenha.value === "") { //campos vazios
+    resultado.textContent = "Preencha todos os campos!!"
+    resultado.classList.add("erro")
+} else if (idade < 18) {
+    resultado.textContent = "Login inválido: Menor de idade."
+    resultado.classList.add("erro")
+} else if (senha !== "1234") {
+    resultado.textContent = "Login inválido: Senha incorreta."
+    resultado.classList.add("erro")
+} else {
+    resultado.textContent = "Acesso Liberado!"
+    resultado.classList.add("sucesso")
+}
+})
+   
